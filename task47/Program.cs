fb84e4dfaae81add251a228271789a7f48a9d40d
Serg <patron.serg@yandex.ru>
2023-06-05 10:58:51 +0300

    f

---
+++ task47/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+// m = 3, n = 4.
+
+// 0,5 7 -2 -0,2
+
+// 1 -3,3 8 -9,9
+
+// 8 7,8 -7,1 9
+
+Console.Write("Введите количество строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите количество столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] myArray = new double[m, n];
+
+Random rnd = new Random();
+for (int i = 0; i < m; i++) {
+      for (int j = 0; j < n; j++) {
+        myArray[i, j] = Math.Round(rnd.NextDouble() * 20 - 10, 1);
+      }
+    }
+
+Console.WriteLine("Ваш массив:");
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(myArray[i, j] + " ");
+    }
+    Console.WriteLine();
+}
---
+++ task50/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 50. Напишите программу, которая на вход принимает значение элемента в двумерном массиве, и возвращает позицию этого элемента или же указание, что такого элемента нет.
+
+// Например, задан массив:
+
+// 1 4 7 2
+
+// 5 9 2 3
+
+// 8 4 2 4
+
+// 17 -> такого числа в массиве нет
+
+int[,] array = new int[,] { { 1, 4, 7, 2 }, { 5, 9, 2, 3 }, { 8, 4, 2, 4 } };
+Console.WriteLine("Введите значение элемента: ");
+int value = Convert.ToInt32(Console.ReadLine());
+int row = -1;
+int column = -1;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        if (array[i, j] == value)
+        {
+            row = i;
+            column = j;
+            break;
+        }
+    }
+}
+if (row != -1 && column != -1)
+{
+    Console.WriteLine($"Позиция элемента: [{row},{column}]");
+}
+else
+{
+    Console.WriteLine("Такого элемента нет в массиве.");
+}
